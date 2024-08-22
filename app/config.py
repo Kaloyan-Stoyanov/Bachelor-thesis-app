1833137912904c158b1af5ac8e0278f880c9f069
Kaloyan-Stoyanov <79645779+Kaloyan-Stoyanov@users.noreply.github.com>
2024-08-22 08:35:09 +0000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,11 +1,8 @@
 # config.py
 
 class Config:
-    # Base configuration
     SECRET_KEY = 'your_secret_key'
-    # Other configurations...
 
 class TestingConfig(Config):
     TESTING = True
     DEBUG = True
-    # Add other test-specific configurations...
